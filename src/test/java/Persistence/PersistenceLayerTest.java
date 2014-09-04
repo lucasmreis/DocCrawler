340e5b46a9b108612efcb361920f4050c3e07c91
lucasmreis <lucasmreis@gmail.com>
2014-09-04 19:33:33 -0300

    melhorei teste PersistenceLayer

--- src/test/java/Persistence/PersistenceLayerTest.java
+++ src/test/java/Persistence/PersistenceLayerTest.java
@@ -4,9 +4,14 @@ import Models.CrawledDoc;
 import Models.CrawledDocBuilder;
 import junit.framework.TestCase;
 import org.joda.time.DateTime;
+import org.junit.After;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,5 +53,40 @@ public class PersistenceLayerTest extends TestCase {
 		String actual = db.Add(doc);
 
 		assertThat(actual).isEqualTo(doc.getId_document());
+
+		Boolean couldEraseBack = eraseDocAddedByTest();
+		assertThat(couldEraseBack).isTrue();
+	}
+
+	public Boolean eraseDocAddedByTest() throws SQLException {
+		String dbURL = "jdbc:mysql://localhost:3306/crawler";
+		String username ="newuser";
+		String password = "password";
+
+		Connection dbCon;
+		Statement stmt;
+		ResultSet rs;
+
+		dbCon = DriverManager.getConnection(dbURL, username, password);
+		stmt = dbCon.createStatement();
+
+		String selectQuery = "SELECT id_document FROM document WHERE id_document = \"IMPORTANT_ID\"";
+		rs = stmt.executeQuery(selectQuery);
+
+		Boolean result = false;
+		if (rs.next()) result = true;
+
+		String deleteQuery = "DELETE FROM `document` WHERE id_document = \"IMPORTANT_ID\"";
+		stmt.executeUpdate(deleteQuery);
+
+		rs = stmt.executeQuery(selectQuery);
+
+		if (rs.next()) result = false;
+
+		rs.close();
+		stmt.close();
+		dbCon.close();
+
+		return result;
 	}
 }
