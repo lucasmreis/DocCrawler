865c87b1bf8c11582324c3fc80db2c9b2ac7b4c2
lucasmreis <lucasmreis@gmail.com>
2014-09-04 13:53:12 -0300

    teste de insertQuery passando

--- src/main/java/Persistence/PersistenceLayer.java
+++ src/main/java/Persistence/PersistenceLayer.java
@@ -2,11 +2,51 @@ package Persistence;
 
 import Models.CrawledDoc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by lucasmreis on 9/4/14.
  */
 public class PersistenceLayer {
-	public String Add(CrawledDoc doc) {
+	public String Add(CrawledDoc doc) throws SQLException {
+		String dbURL = "jdbc:mysql://localhost:3306/crawler";
+		String username ="root";
+		String password = "root";
+
+		Connection dbCon = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String query = insertQuery(doc);
+
+		//dbCon = DriverManager.getConnection(dbURL, username, password);
+
+
+
 		return null; // TODO: return id_document if successful.
+	}
+
+	public String insertQuery(CrawledDoc doc)
+	{
+		return "INSERT INTO `document` (" +
+				"`id_document`, " +
+				"`title`, " +
+				"`content`, " +
+				"`author`, " +
+				"`date`, " +
+				"`link`, " +
+				"`source`) " +
+				"VALUES ( \"" +
+				doc.getId_document() + "\", \"" +
+				doc.getTitle() + "\", \"" +
+				doc.getContent() + "\", \"" +
+				doc.getAuthor() + "\", \"" +
+				doc.getDate().toString("yyyy-MM-dd") + "\", \"" +
+				doc.getLink() + "\", \"" +
+				doc.getSource() + "\");";
 	}
 }
--- src/test/java/Persistence/PersistenceLayerTest.java
+++ src/test/java/Persistence/PersistenceLayerTest.java
@@ -6,11 +6,34 @@ import junit.framework.TestCase;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PersistenceLayerTest extends TestCase {
 	@Test
-	public void testPersistenceHasToAddProperly() {
+	public void testMustBuildCorrectInsertQuery() {
+		CrawledDoc doc = CrawledDocBuilder.newCrawledDoc()
+				.withAuthor("author")
+				.withContent("content")
+				.withDate(new DateTime(1982, 11, 17, 12, 00))
+				.withLink("link")
+				.withSource("source")
+				.withTitle("title")
+				.withId_document("IMPORTANT_ID")
+				.create();
+
+		PersistenceLayer db = new PersistenceLayer();
+		String query = db.insertQuery(doc);
+
+		assertThat(query).isEqualTo(
+				"INSERT INTO `document` (`id_document`, `title`, `content`, `author`, `date`, `link`, `source`) " +
+				"VALUES ( \"IMPORTANT_ID\", \"title\", \"content\", \"author\", \"1982-11-17\", \"link\", \"source\");"
+		);
+	}
+
+	@Test
+	public void testPersistenceHasToAddProperly() throws SQLException {
 		CrawledDoc doc = CrawledDocBuilder.newCrawledDoc()
 				.withAuthor("author")
 				.withContent("content")
