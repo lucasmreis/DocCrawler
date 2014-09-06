3e703f0a9c8ccbb27a284efd4cdc2a51cb386868
lucasmreis <lucasmreis@gmail.com>
2014-09-06 00:29:46 -0300

    testes para RssEngine

--- src/main/java/Engines/RssEngine.java
+++ src/main/java/Engines/RssEngine.java
@@ -1,6 +1,7 @@
 package Engines;
 
 import Models.CrawledDoc;
+import Models.CrawledDocBuilder;
 import com.sun.syndication.feed.synd.SyndEntry;
 
 /**
@@ -10,5 +11,11 @@ public class RssEngine {
 	public CrawledDoc getCrawledDoc(SyndEntry entry)
 	{
 		return null;
+//				CrawledDocBuilder.newCrawledDoc()
+//				.withSource("Rss Feed")
+//				.withAuthor(entry.getAuthor())
+//				.withLink(entry.getUri().toString())
+//				.withTitle(entry.getTitle())
+//				.withContent(entry.getContents())
 	}
 }
---
+++ src/test/java/Engines/RssEngineTest.java
@@ -0,0 +1,40 @@
+package Engines;
+
+import Models.CrawledDoc;
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+public class RssEngineTest extends TestCase {
+	@Test
+	public void testRssEngineHasToWorkProperly() {
+		SyndEntry entry = new SyndEntryImpl();
+		entry.setAuthor("author");
+		entry.setLink("link");
+		entry.setTitle("title");
+
+		SyndContent content = new SyndContentImpl();
+		content.setType("text/plain");
+		content.setValue("contents\ncontents");
+		entry.setContents(Arrays.asList(content));
+
+		RssEngine engine = new RssEngine();
+
+		CrawledDoc doc = engine.getCrawledDoc(entry);
+
+		assertThat(doc).isNotNull();
+		assertThat(doc.getId_document()).isNotNull();
+		assertThat(doc.getId_document()).isNotEmpty();
+		assertThat(doc.getTitle()).isEqualTo("title");
+		assertThat(doc.getLink()).isEqualTo("link");
+		assertThat(doc.getAuthor()).isEqualTo("author");
+		assertThat(doc.getContent()).isEqualTo("contents\ncontents");
+	}
+}
